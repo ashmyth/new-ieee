2849688ddb846c2ef5e47f600c3fcb485e0e5785
ashmyth <ashmyth@users.noreply.github.com>
2026-08-29 15:24:35 +0530

    style: move hero text down by 2px

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -151,7 +151,7 @@ export const Hero: React.FC = () => {
             className="absolute z-20 flex items-center h-[51px]"
             style={{ 
               left: 'calc(clamp(50px, 21.1vw, 83px) + 1px)', 
-              top: '361px',
+              top: '363px',
             }}
           >
             <span 
@@ -169,7 +169,7 @@ export const Hero: React.FC = () => {
             className="absolute z-20 flex items-center h-[49px] gap-[17px]"
             style={{ 
               left: '8px', 
-              top: '414px',
+              top: '416px',
             }}
           >
             <span 
@@ -296,7 +296,7 @@ export const Hero: React.FC = () => {
             className="absolute z-20 flex items-center h-[134px]"
             style={{ 
               left: 'calc(50% - 235px)', 
-              top: 'calc(50% - 137px)' 
+              top: 'calc(50% - 135px)' 
             }}
           >
             <span 
@@ -315,7 +315,7 @@ export const Hero: React.FC = () => {
             className="absolute z-20 flex items-center h-[131px] gap-[57px]"
             style={{ 
               left: 'calc(50% - 518px)', 
-              top: 'calc(50% + 1px)' 
+              top: 'calc(50% + 3px)' 
             }}
           >
             <span 
