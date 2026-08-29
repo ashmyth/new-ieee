0a6dafb376b2113a3ab8a8c0dce6cbe58eae9c18
ashmyth <ashmyth@users.noreply.github.com>
2026-08-29 15:12:19 +0530

    style: increase word space between For and Engineers

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -166,7 +166,7 @@ export const Hero: React.FC = () => {
           </div>
 
           <div 
-            className="absolute z-20 flex items-center h-[51px] gap-[14px]"
+            className="absolute z-20 flex items-center h-[51px] gap-[18px]"
             style={{ 
               left: '8px', 
               top: '419px',
@@ -312,7 +312,7 @@ export const Hero: React.FC = () => {
 
           {/* Row 2: "For Engineers" */}
           <div 
-            className="absolute z-20 flex items-center h-[136px] gap-[42px]"
+            className="absolute z-20 flex items-center h-[136px] gap-[58px]"
             style={{ 
               left: 'calc(50% - 518px)', 
               top: 'calc(50% + 4px)' 
